0721c44c8d5cf41b8b24fcef4c4013ef06991934
Mikael Storman Gustavsson <miga02@gmail.com>
2022-09-19 19:22:22 +0200

    Httpclient retry with Polly.

--- BlazorAppWithFluxor/Client/Program.cs
+++ BlazorAppWithFluxor/Client/Program.cs
@@ -4,12 +4,23 @@ using Blazored.Toast;
 using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Polly;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Using polly to retry failed network calls.
+builder.Services
+    .AddHttpClient("Default", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+    {
+        TimeSpan.FromSeconds(1),
+        TimeSpan.FromSeconds(5)
+    }));
+
+// Create default HttpClient 
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>()!.CreateClient("Default"));
 
 builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly));
 
--- BlazorAppWithFluxor/Server/Controllers/WeatherForecastController.cs
+++ BlazorAppWithFluxor/Server/Controllers/WeatherForecastController.cs
@@ -3,32 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorAppWithFluxor.Server.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+      _logger = logger;
     }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+      if (DateTime.Now.Second % 2 == 0) { throw new Exception("This is on purpose!"); }
+
+      return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+      {
+        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        TemperatureC = Random.Shared.Next(-20, 55),
+        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+      })
+      .ToArray();
+    }
+  }
 }
